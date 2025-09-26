c48fad20953ca2f9d6eae6a6ffb69abbddae50c9
cuijustin0617 <cuijustin0617@gmail.com>
2025-09-25 20:42:21 -0400

    small changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cuijustin0617.github.io',
-  base: '/website',
+  base: '/',
   outDir: 'docs',
   build: {
     assets: '_astro'
