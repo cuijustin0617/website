5b502f1a97cc5ef6f2405ec12e01311c2288d9f0
cuijustin0617 <cuijustin0617@gmail.com>
2025-09-25 20:01:03 -0400

    debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://cuijustin0617.github.io',
   base: '/website',
+  outDir: 'docs',
   build: {
     assets: '_astro'
   }
